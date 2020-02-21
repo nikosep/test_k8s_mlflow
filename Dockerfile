c7a105f04983c71f0b85f9308de62eeb1c5faf00
nikosep <n.epitro@gmail.com>
2020-02-21 14:30:17 +0200

    testing MLflow

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ COPY requirements.txt /opt/
 WORKDIR /opt/
 RUN pip3 install -r requirements.txt
 ADD . /opt
+RUN export export MLFLOW_TRACKING_URI='http://172.16.2.20:8000/'
 CMD [ "python3", "./main.py" ]
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import os
+from random import random, randint
+from mlflow import log_metric, log_param, log_artifacts
 
-data = pd.DataFrame({"A": ["John", "Deep", "Julia", "Kate", "Sandy"],
-                     "MonthSales": [25, 30, 35, 40, 45]})
-print(data.head())
+if __name__ == "__main__":
+    print("Running mlflow_tracking.py")
+    log_param("param1", randint(0, 100))
+    log_metric("foo", random())
+    log_metric("foo", random() + 1)
+    log_metric("foo", random() + 2)
+    if not os.path.exists("outputs"):
+        os.makedirs("outputs")
+    with open("outputs/test.txt", "w") as f:
+        f.write("hello world!")
+    log_artifacts("outputs")
